e00a73bb9c710ecc5d58558c4dfc4f54526c5666
mayurgpt07 <mayurgpt07@gmail.com>
2017-03-04 00:18:10 +0530

    Changes

--- public/js/data.factory.js
+++ public/js/data.factory.js
@@ -3,7 +3,7 @@
 
         let apiData = function() {
             console.log('In API');
-            return $http.get('http://tripshire1.herokuapp.com/feeds');
+            return $http.get('https://tripshire1.herokuapp.com/feeds');
         };
 
         // let facebook = () => {
