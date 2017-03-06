20bd557501976de2dd1ebe4cca6fc2b696c5e865
pulkitkkr <pulkitkkr@gmail.com>
2017-03-07 02:11:48 +0530

    Page 3 Shows All data now

--- public/js/page3.ctrl.js
+++ public/js/page3.ctrl.js
@@ -3,6 +3,13 @@ angular.module('page3').controller('page3Ctrl', ($scope,$rootScope, $http, dataF
     $scope.data = '';
     $scope.cards = null;
     $scope.activities = new Activities();
+    $scope.logger = function(ind){
+       var showable = $scope.activities.items[ind];
+            console.log(showable);
+            $window.localStorage.setItem('Selected',JSON.stringify(showable));
+               $window.location.href = '/page3';
+    }
+    
     console.log($scope.activities);
     $scope.SelectedPlace = JSON.parse($window.localStorage.getItem('Selected'));
     $scope.datax = JSON.stringify( $scope.SelectedPlace);
